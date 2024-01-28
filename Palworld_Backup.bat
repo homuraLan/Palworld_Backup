e0379b3bfa7a6d5574ae2974a88d77a6b28b498b
homura <943384135@qq.com>
2024-01-28 20:17:28 +0800

    优化

--- Palworld_Backup.bat
+++ Palworld_Backup.bat
@@ -6,8 +6,8 @@ rem 设置游戏路径
 set "palworld_path=C:\Steam\steamapps\common\PalServer"
 rem 设置备份路径
 set "backup_path=C:\gameServer\PalServer"
-
-set "datetime.txt=datetime.txt"
+rem 设置保存最近的多少个存档
+set "less_save=5"
 
 rem 设置时间阈值（可以指定小时、天、月、年和分钟）
 set "time_threshold_hours=2"
@@ -40,34 +40,39 @@ set foldername=%current_year%-%current_month%-%current_day%_%current_hour%-%curr
 
 echo 当前日期时间：!current_year!-!current_month!-!current_day! !current_hour!:!current_minute!:!current_second!
 
-if not exist "%datetime.txt%" (
-(
-  echo !current_year!-!current_month!-!current_day! !current_hour!:!current_minute!:!current_second!
-) > datetime.txt)
-
-set /p datetime=<datetime.txt
-rem 解析日期和时间
-for /f "tokens=1-6 delims=-: " %%a in ("%datetime%") do (
-  set "saved_year=%%a"
-  set "saved_month=%%b"
-  set "saved_day=%%c"
-  set "saved_hour=%%d"
-  set "saved_minute=%%e"
-  set "saved_second=%%f"
-)
-
-
 xcopy "%palworld_path%\Pal\Saved" "%backup_path%\Backup_%foldername%" /E /H /C /I
 
 rem 转换时间阈值为分钟
 set /a "total_threshold_minutes = time_threshold_years * 525600 + time_threshold_months * 43800 + time_threshold_days * 1440 + time_threshold_hours * 60 + time_threshold_minutes"
 
+set /a backup_count=0
+
+for /d %%i in ("%backup_path%\*") do (
+    set "folder_name=%%~nxi"
+    echo !folder_name! | findstr /r "^Backup_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9].*$" > nul
+    if !errorlevel! equ 0 (
+        set /a backup_count+=1
+    )
+)
+set /a "backup_count=backup_count-less_save"
+
+if !backup_count! leq 0 (
+    goto endDelete
+)
+
+set /a count=0
 rem 遍历指定文件夹
 for /d %%i in ("%backup_path%\*") do (
     rem 提取文件夹名称中的日期时间信息
     set "folder_name=%%~nxi"
     echo !folder_name! | findstr /r "^Backup_[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]-[0-9][0-9]-[0-9][0-9].*$" > nul
     if !errorlevel! equ 0 (
+        set /a count+=1
+        if !count! geq %backup_count% (
+            echo [%date% %time%] 保存最近至少 !less_save! 个存档
+            goto :endDelete
+        )
+
         set "file_year=!folder_name:~7,4!"
         set "file_month=!folder_name:~12,2!"
         set "file_day=!folder_name:~15,2!"
@@ -75,15 +80,15 @@ for /d %%i in ("%backup_path%\*") do (
         set "file_minute=!folder_name:~21,2!"
         
         rem 计算日期和时间差值
-        set /a "diff_year=saved_year-file_year"
-        set /a "diff_month=saved_month-file_month"
-        set /a "diff_day=saved_day-file_day"
-        set /a "diff_hour=saved_hour-file_hour"
-        if !diff_hour! lss 0 set /a "diff_hour=saved_hour+24-file_hour" & set /a "diff_day=diff_day-1"
-        set /a "diff_minute=saved_minute-file_minute"
-        if !diff_minute! lss 0 set /a "diff_minute=saved_minute+60-file_minute" & set /a "diff_hour=diff_hour-1"
-        set /a "diff_second=saved_second-file_second"
-        if !diff_second! lss 0 set /a "diff_second=saved_second+60-file_second" & set /a "diff_minute=diff_minute-1"
+        set /a "diff_year=current_year-file_year"
+        set /a "diff_month=current_month-file_month"
+        set /a "diff_day=current_day-file_day"
+        set /a "diff_hour=current_hour-file_hour"
+        if !diff_hour! lss 0 set /a "diff_hour=current_hour+24-file_hour" & set /a "diff_day=diff_day-1"
+        set /a "diff_minute=current_minute-file_minute"
+        if !diff_minute! lss 0 set /a "diff_minute=current_minute+60-file_minute" & set /a "diff_hour=diff_hour-1"
+        set /a "diff_second=current_second-file_second"
+        if !diff_second! lss 0 set /a "diff_second=current_second+60-file_second" & set /a "diff_minute=diff_minute-1"
 
         rem 转换时间差为分钟
         set /a "total_diff_minutes = diff_year * 525600 + diff_month * 43800 + diff_day * 1440 + diff_hour * 60 + diff_minute"
@@ -98,6 +103,8 @@ for /d %%i in ("%backup_path%\*") do (
         )
     )
 )
+
+:endDelete
 
 timeout /t %interval%
 
